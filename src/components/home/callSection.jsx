b05fe9a98c03e552345a8b42af792570c51f0ee0
Abbas <mrghulamabbas10@gmail.com>
2025-07-02 22:16:16 +0500

    cahge done

--- src/components/home/callSection.jsx
+++ src/components/home/callSection.jsx
@@ -17,12 +17,12 @@ export default function CallSection() {
   return (
     <div className="md:block hidden pt-32 pb-20 space-y-5 overflow-hidden calltoaction md:px-8 px-3" id="book">
       {/* Main Heading */}
-      <h2 className="text-4xl sm:text-4xl text-primary capitalize font-bold mb-5 text-center">
+      <h2 className="text-2xl sm:text-4xl text-primary capitalize font-bold mb-5 text-center">
         Convenient Online Consultation with <br className="md:block hidden" />
         <span className="text-burgundy">Licensed 👩🏻‍⚕️ Providers</span>
       </h2>
 
-      <Link  href="/eligibility" className="mt-10 relative z-10 mx-auto flex items-center justify-center px-8 border md:w-fit w-full border-primary text-primary font-semibold py-3 rounded-full hover:bg-primary hover:text-white transition">
+      <Link  href="/eligibility" className="md:mt-10 mt-3 relative z-10 mx-auto flex items-center justify-center px-8 border md:w-fit w-full border-primary text-primary font-semibold py-3 rounded-full hover:bg-primary hover:text-white transition">
      Book Free Consultation
       </Link>
 
@@ -31,15 +31,16 @@ export default function CallSection() {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true }}
-        className="-mt-20"
+        className="md:-mt-20"
       >
         <Image
-          src="/images/call-sec.png"
+          src="/images/Video-consultation.png"
           alt="call-sec"
           width={1320}
           height={816}
           className="w-full h-full object-center"
-          quality={100}
+          quality={50}
+          
         />
       </motion.div>
     </div>
