fc28318657669c5be2af1e45c1a22370c375e893
Abbas <122156175+mrghulamabbas10@users.noreply.github.com>
2025-07-25 20:57:39 +0500

    padding issue

--- src/components/home/ScrollSection.jsx
+++ src/components/home/ScrollSection.jsx
@@ -36,11 +36,10 @@ const docterData = [
 ];
 
 export default function ScrollSection() {
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <div className="Subtractbg relative flex md:flex-row flex-col gap-5 items-center justify-between md:px-8 px-0 pb-5 md:mt-0  mt-10">
+    <div className="Subtractbg relative flex md:flex-row flex-col gap-5 items-center justify-between md:px-8 px-0 md:pt-5 pt-0  pb-5 md:mt-0 mt-10">
       <div className="space-y-1 pt-12 pl-5 md:hidden flex-col gap-2 items-center justify-center flex">
         <div className="p-2 flex items-center justify-center mx-auto rounded-full bg-white text-primary">
           <GoArrowDown />
@@ -65,7 +64,10 @@ export default function ScrollSection() {
         className="xl:left-[47vw] left-[42.5vw] absolute md:top-0 -top-[50px]"
       />
 
-      <div onClick={()=>router.push("#plans")} className="space-y-3 cursor-pointer pt-20 md:left-[66px] md:flex flex-col gap-2 items-center justify-center hidden">
+      <div
+        onClick={() => router.push("#plans")}
+        className="space-y-3 cursor-pointer pt-20 md:left-[66px] md:flex flex-col gap-2 items-center justify-center hidden"
+      >
         <div className="p-2 mx-auto rounded-full bg-white text-primary text-2xl block w-fit">
           <GoArrowDown />
         </div>
